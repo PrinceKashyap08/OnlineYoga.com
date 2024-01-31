9e9951f7c06d4ab5bf6b0609f3153785c1fe85d9
Prince <vasukashyap08@gmail.com>
2024-01-31 20:10:39 +0530

    commit

---
+++ main.js
@@ -0,0 +1,16 @@
+$('#hero .owl-carousel').owlCarousel({
+    loop:true,
+    dots: true,
+    nav: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
