50e072cd17387dddc5c6e262b5dda4aa4cf490ae
Prince <vasukashyap08@gmail.com>
2024-02-01 15:29:02 +0530

    commit

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ $('#hero .owl-carousel').owlCarousel({
     loop:true,
     dots: true,
     nav: false,
+    autoplay: true,
+    loop: true,
     responsive:{
         0:{
             items:1
@@ -13,4 +15,16 @@ $('#hero .owl-carousel').owlCarousel({
             items:1
         }
     }
-})
+});
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+hamburger.addEventListener("click", mobileMneu)
+
+function mobileMneu(){
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+};
+
+AOS.init();
+
